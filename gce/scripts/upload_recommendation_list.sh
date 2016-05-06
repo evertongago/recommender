caaa554af94f9c61dcd8dfb5ae2d2b6ad4dda4c6
Everton Gago <everton.gago@dextra-sw.com>
2016-05-06 12:36:28 -0300

    Initial Commit

--- gce/scripts/upload_recommendation_list.sh
+++ gce/scripts/upload_recommendation_list.sh
@@ -2,4 +2,4 @@
 
 dsopz login-gce
 
-cat /root/data/recommendation_list.data | awk '{printf "{\"properties\": {\"user_id\": {\"indexed\": true, \"integerValue\": %s}, \"content_id\": {\"indexed\": false, \"stringValue\": %s}, \"rate\": {\"indexed\": true, \"doubleValue\": %s}}, \"key\": { \"path\": [{\"kind\": \"RecommendationContent\", \"name\": \"%s\"}]}}\n", $2, $3, $4, $1}' | dsopz import -d gadsense-1218 -o upsert
+cat /root/data/recommendation_list.data | awk '{printf "{\"properties\": {\"user_id\": {\"indexed\": true, \"integerValue\": %s}, \"content_id\": {\"indexed\": false, \"stringValue\": %s}, \"rate\": {\"indexed\": true, \"doubleValue\": %s}}, \"key\": { \"path\": [{\"kind\": \"RecommendationContent\", \"name\": \"%s\"}]}}\n", $2, $3, $4, $1}' | dsopz import -d <project-id> -o upsert
